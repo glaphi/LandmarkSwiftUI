f241d02b58db54d18c898e28467b60b8c2844de5
glaphi <glafira.privalova@dreambroker.fi>
2019-06-04 11:22:24 +0300

    MapView representation of MKMapView

--- LandmarkSwiftUI/CircleImage.swift
+++ LandmarkSwiftUI/CircleImage.swift
@@ -14,7 +14,6 @@ struct CircleImage : View {
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 10)
-
     }
 }
 
---
+++ LandmarkSwiftUI/MapView.swift
@@ -0,0 +1,34 @@
+//
+//  MapView.swift
+//  LandmarkSwiftUI
+//
+//  Created by Glafira Privalova on 4.6.2019.
+//  Copyright © 2019 com.glaphi. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    typealias UIViewType = MKMapView
+
+    func makeUIView(context: Context) -> MapView.UIViewType {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
