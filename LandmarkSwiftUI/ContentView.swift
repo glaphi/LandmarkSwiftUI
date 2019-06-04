900a5616859a23f047a0a48becb0a3a6e57da6a8
glaphi <glafira.privalova@dreambroker.fi>
2019-06-04 11:30:04 +0300

    ContentView with mapView and CircleImage

--- LandmarkSwiftUI/ContentView.swift
+++ LandmarkSwiftUI/ContentView.swift
@@ -10,19 +10,26 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
+        VStack {
 
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
+            MapView().frame(height: 300).edgesIgnoringSafeArea(.top)
+
+            CircleImage().offset(y: -130).padding(.bottom, -130)
+
+
+            VStack(alignment: .leading) {
+                    Text("Turtle Rock").font(.title)
+
+                    HStack {
+                        Text("Joshua Tree National Park").font(.subheadline)
+                        Spacer()
+                        Text("California").font(.subheadline)
+                    }
+                }
+                .padding()
+
+            Spacer()
         }
-        .padding()
     }
 
 }
