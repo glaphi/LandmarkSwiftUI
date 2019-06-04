42f8ec8340bf35aefb1beef6144aab3a58e3edcb
glaphi <glafira.privalova@dreambroker.fi>
2019-06-04 10:55:24 +0300

    First row and init circleView

---
+++ LandmarkSwiftUI/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandmarkSwiftUI
+//
+//  Created by Glafira Privalova on 4.6.2019.
+//  Copyright © 2019 com.glaphi. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
--- LandmarkSwiftUI/ContentView.swift
+++ LandmarkSwiftUI/ContentView.swift
@@ -10,8 +10,21 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
+
 }
 
 #if DEBUG
