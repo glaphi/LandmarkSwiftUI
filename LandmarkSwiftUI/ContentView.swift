1e8549b6719f69d982a99b3efe58fa9463a9922e
glaphi <glafira.privalova@dreambroker.fi>
2019-06-04 10:27:02 +0300

    Initial Commit

---
+++ LandmarkSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LandmarkSwiftUI
+//
+//  Created by Glafira Privalova on 4.6.2019.
+//  Copyright © 2019 com.glaphi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
