f499f8951434be805b760719b6c3627a30c3e768
glaphi <glafira.privalova@dreambroker.fi>
2019-06-04 12:44:20 +0300

    Landmark list from identifiable data

---
+++ LandmarkSwiftUI/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  LandmarkSwiftUI
+//
+//  Created by Glafira Privalova on 4.6.2019.
+//  Copyright © 2019 com.glaphi. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+
+        // Lists work with identifiable data.
+        // You can make your data identifiable in one of two ways:
+        // - by calling the identified(by:) method with a key path to a property that uniquely identifies each element
+        // - by making your data type conform to the Identifiable protocol.
+
+        List(landmarkData.identified(by: \.id)) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
+#endif
